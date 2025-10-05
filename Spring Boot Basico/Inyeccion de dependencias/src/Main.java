02484d5a2accdd05c96e28b077856e2ddd6fd845
Sebastian <macbook@MacBook-Air-de-Macbook.local>
2025-10-04 20:10:58 -0500

    update

--- Spring Boot Basico/Inyeccion de dependencias/src/Main.java
+++ Spring Boot Basico/Inyeccion de dependencias/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        IMotor motor = new MotorElectrico(); // O MotorElectrico()
-        Carro car1 = new Carro(motor);    // Inyección aquí
-        car1.arrancar();                  // 🚀
+        IMotor motor = new MotorElectrico(); //
+        IMotor motor2 = new MotorGasolina();
+        Carro car1 = new Carro(motor);  // Inyeccion aqui
+        Carro car2 = new Carro(motor2);// Inyección aquí
+        car1.arrancar();
+        car2.arrancar();
     }
 }
